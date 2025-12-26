b0cc6002ff4394742cc8fa5340c6fd3639138fef
bala-amypo <bala@amypo.in>
2025-12-26 19:16:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Userserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Userserviceimpl.java
@@ -3,13 +3,13 @@ import com.example.demo.service.UserService;
 import com.example.demo.entity.User;
 import com.example.demo.repository.Userrepository;
 import org.springframework.beans.factory.annotation.Autowired;
-class Userserviceimpl implements Userservice{
-@Autowired
+class Userserviceimpl implements UserService{
+    @Autowired
     Userrepository obj;
     public User getById(Long id){
         return obj.findById(id).orElse(null);
     }
-    User create(User data){
+    public User create(User data){
         return obj.save(data);
     }
 }
