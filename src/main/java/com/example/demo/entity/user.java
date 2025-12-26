936032765251548708739a0a663ea24a870a97a3
bala-amypo <bala@amypo.in>
2025-12-26 18:30:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/user.java
+++ src/main/java/com/example/demo/entity/user.java
@@ -2,7 +2,24 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 class user{
-    private String Name;
-    @unique()
+    private String name;
+    @Column(unique=true)
     private String email;
+    String getName(){
+        return name;
+    }
+    String setName(String name){
+        this.name=name;
+    }
+    String getEmail(){
+        return email;
+    }
+    String setEmail(String email){
+        this.email=email;
+    }
+    public user(){}
+    public user(String name,String email){
+        this.name=name;
+        this.email=email;
+    }
 }
